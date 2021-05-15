91e7402a4532e0df8842c953efb146402d190739
Sayan Biswas <sayan@pokurt.me>
2021-05-15 22:14:32 +0530

    core: rework the startup process.

--- kanri/__init__.py
+++ kanri/__init__.py
@@ -1,32 +1,34 @@
 from configparser import ConfigParser
 from pyrogram import Client
-import logging
+import logging, threading
 
-FORMAT = "%(message)s"
+FORMAT = "[KanRi] %(message)s"
+
+KLOCAL = threading.local()
 
 logging.basicConfig(level=logging.INFO, format=FORMAT, datefmt="[%X]")
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
-LOGGER = logging.getLogger(__name__)
+KLOCAL.LOGGER = logging.getLogger(__name__)
 
-LOGGER.info("Kanri is starting..........")
+KLOCAL.LOGGER.info("Kanri is starting..........")
 
 parser = ConfigParser()
 parser.read("config.ini")
 
 kanriconf = parser["kanriconf"]
-owner_id = kanriconf.getint("owner_id")
+KLOCAL.owner_id = kanriconf.getint("owner_id")
 dev_users = kanriconf.get("dev_users")
-dev_users = list(map(int, dev_users))
+KLOCAL.dev_users = list(map(int, dev_users))
 operators = kanriconf.get("operators")
-operators = list(map(int, operators))
-postgres_url = kanriconf.get("postgres_url")
-CF_API_KEY = kanriconf.get("CF_API_KEY", None)
+KLOCAL.operators = list(map(int, operators))
+KLOCAL.postgres_url = kanriconf.get("postgres_url")
+KLOCAL.CF_API_KEY = kanriconf.get("CF_API_KEY", None)
 
 plugins = parser["plugins"]
 include = plugins.get("include")
-LOAD = list(map(str, include))
+KLOCAL.LOAD = list(map(str, include))
 exclude = plugins.get("exclude")
-NO_LOAD = list(map(str, exclude))
+KLOCAL.NO_LOAD = list(map(str, exclude))
 
-kanri = Client(":memory:", config_file="config.ini")
+KLOCAL.client = Client(":memory:", config_file="config.ini")
--- kanri/__main__.py
+++ kanri/__main__.py
@@ -1,9 +1,55 @@
-from pyrogram import idle
-from kanri import kanri, LOGGER
+from pyrogram.session import Session
+from pyrogram import __version__, idle
+from kanri import KLOCAL
+import asyncio, logging, os, importlib
+
+
+async def load():
+    if os.name != 'nt':
+        path = 'kanri/plugins'
+    else:
+        path = 'kanri\plugins'
+
+    for x in os.listdir(path):
+        if hasattr(x, 'name'):
+            KLOCAL.LOGGER.info(f'[LOADER] Found path {x.name}')
+        try:
+            for y in os.listdir(x):
+                if hasattr(y, 'name'):
+                    if y.name.endswith('.py'):
+                        importlib.import_module(name=y, package=x)
+                        KLOCAL.LOGGER.info(f'[LOADER] Loading {y.name} from package {x.name}')
+        except BaseException:
+            pass
+
+
+async def run_kanri():
+    '''
+    Kanri's asynchronous entry point, all startup events are initalised here.
+    '''
+    try:
+        await load()
+        KLOCAL.LOGGER.info("Starting Kanri's Pyrogram Client")
+        await KLOCAL.client.start()
+        KLOCAL.LOGGER.info("Kanri started!")
+        await idle()
+    except BaseException as error:
+        KLOCAL.LOGGER.exception(f"Exiting due to a {type(error).__name__}: {error}")
+    finally:
+        KLOCAL.LOGGER.warning("Kanri is shutting down")
 
 
 if __name__ == "__main__":
-    kanri.start()
-    LOGGER.info("Kanri started!")
-    idle()
-
+    KLOCAL.LOGGER.info(
+        f"""Starting Kanri, powered by Pyrogram (v{__version__}). Copyright (C) 2021 Intellivoid Technologies
+    This program comes with ABSOLUTELY NO WARRANTY.
+    This is free software, and you are welcome to redistribute it
+    under certain conditions, check the LICENSE file for more information."""
+    )
+    KLOCAL.LOGGER.info("Initializing Kanri")
+    # Sets pyrogram logging to warning because info is too verbose
+    KLOCAL.LOGGER.info("Setting pyrogram logs to warning to avoid verbose output and disabling the session notice")
+    logging.getLogger("pyrogram").setLevel(logging.WARNING)
+    Session.notice_displayed = True
+    KLOCAL.LOGGER.info("Starting asyncio event loop")
+    asyncio.get_event_loop().run_until_complete(run_kanri())
